b31732645c62b57a4f371712908cf5c5fd3bed87
shelby11112 <78622957+shelby11112@users.noreply.github.com>
2022-01-17 20:26:45 +0300

    Update black.js

--- black.js
+++ black.js
@@ -31,7 +31,7 @@ const Enmap = require("enmap");
 const cooldown = new Set();
 const cdtime = 5;
 const prefix = "T"
-client.login("OTE2Nzc2NDgzMzY2NzcyNzU2.YavEog.5YH6n9VJUOHlAmcVYXBjgMjrxv4")
+client.login("OTIwODA4MzA5Mjg2NjY2MzMx.YbpvkA.vVetlfnsR-ZDnVy3FbOeJidSmIc")
 
 let channelc = JSON.parse(fs.readFileSync('./channelc.json' , 'utf8'));
 let channeld = JSON.parse(fs.readFileSync('./channeld.json' , 'utf8'));
